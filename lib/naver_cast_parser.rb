1c3b98cd8f44a1ddf6c439bdd1bc1eae5de4cdc3
benjamin <benjamin@kakao.com>
2014-08-31 12:54:49 +0900

    support full contents in the feed again.

--- lib/naver_cast_parser.rb
+++ lib/naver_cast_parser.rb
@@ -22,18 +22,22 @@ def fetch_data(cast_info_h)
     item.title = HTML::FullSanitizer.new.sanitize(link.css('span.info').text)
     item.link = NAVER_CAST_BASE_URI + link.css('a').attr('href')
 
-    contents_uri = NAVER_CAST_BASE_URI + link.css('a[href^="/contents.nhn"]').attr('href')
+    contents_uri = (NAVER_CAST_BASE_URI + link.css('a[href^="/contents.nhn"]').attr('href')).tap do |uri|
+      puts "Content Uri: #{uri}"
+    end
+
     doc = Nokogiri::HTML(open(contents_uri))
     parsed_obj = ''
-    doc.css('div.smarteditor_area.naml_article').each do |article_link|
-      parsed_obj = article_link
-      datetime = article_link.css('div.t_pdate span').text
-      if datetime.blank?
-        item.updated = Time.now.utc.strftime('%FT%T%z')
-      else
-        item.updated = Time.strptime(datetime, "%Y.%m.%d").utc.strftime('%FT%T%z')
-      end
+    article_link = doc.css('div.smarteditor_area.naml_article').first
+    puts "article_link: #{article_link}"
+    parsed_obj = article_link
+    datetime = article_link.css('div.t_pdate span').text
+    if datetime.blank?
+      item.updated = Time.now.utc.strftime('%FT%T%z')
+    else
+      item.updated = Time.strptime(datetime, "%Y.%m.%d").utc.strftime('%FT%T%z')
     end
+
     item.summary = parsed_obj.to_html
     items << item
   end
