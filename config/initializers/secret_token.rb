efc361d4962cc6b49737aa3e70c65a0bc0f6a9b4
Benjamin <benjamin.k@daumkakao.com>
2014-12-29 20:58:47 +0900

    secret_token added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NavercastFeed::Application.config.secret_key_base = '0a132615f3a6426eb1f22ebb98986015878d55bd1706db8fb4b1738e95a7f5ecd1aab1eee0e736dd3919964fa59a136b6d391be34a01bcac0504e707a45a4842'
