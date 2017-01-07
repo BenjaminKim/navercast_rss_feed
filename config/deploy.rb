a5fd9fc0f70020bb0b4415720278c94fd592a27c
Benjamin Kim <jehokim@live.com>
2017-01-07 21:36:20 +0900

    gem updated for ruby 2.4

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.7.0'
+lock '3.7.1'
 
 set :application, 'navercast_feed'
 set :repo_url, 'git@github.com:BenjaminKim/navercast_feed.git'
