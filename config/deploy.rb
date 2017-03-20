f94193f0557f4d4327dd8a7747080c625d344d6b
Benjamin <jehokim@live.com>
2017-03-20 21:29:51 +0900

    puma, capistrano updated.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.7.2'
+lock '3.8.0'
 
 set :application, 'navercast_feed'
 set :repo_url, 'git@github.com:BenjaminKim/navercast_feed.git'
